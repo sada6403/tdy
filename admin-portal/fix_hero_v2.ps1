792a84af0acfca8a7f35daf808c0d7184c7f5dd5
Kulendradas Sabiharan <ksabiharan@gmail.com>
2026-04-22 11:10:04 +0530

    Fix website and admin portal logos and favicons

--- admin-portal/fix_hero_v2.ps1
+++ admin-portal/fix_hero_v2.ps1
@@ -2,3 +2,4 @@ $c = Get-Content "c:\work\nf plantation full project\nf plantation project\admin
 # Add closing div after line 615 (index 614)
 $newContent = $c[0..614] + "                                         </div>" + $c[615..($c.Count-1)]
 $newContent | Set-Content "c:\work\nf plantation full project\nf plantation project\admin-portal\src\pages\HeroManagement.jsx"
+
